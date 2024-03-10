6e6831453147d9d3b628465ad904021a8b8c8287
ALanovaya <lanovaya.al6@gmail.com>
2024-03-10 17:14:26 +0300

    Make tests for different nets

--- Dirichlet/Dirichlet.cpp
+++ Dirichlet/Dirichlet.cpp
@@ -9,8 +9,8 @@ class wavefront_approximator {
     std::vector<std::vector<double>> u;
     std::vector<std::vector<double>> f;
 
-    const int block_size = 32;
-    const double eps = 0.0000001;
+    const int block_size = 16;
+    const double eps = 10e-14;
     double (*fun)(double, double);
     const int nb;
     
@@ -36,26 +36,25 @@ class wavefront_approximator {
 public:
     wavefront_approximator(int size, double (*fun_g)(double, double), double (*fun_f)(double, double)) : size(size), h(1.0 / (size + 1)), fun(fun_g), nb(size / block_size + (size % block_size == 0)) {
         f = std::vector<std::vector<double>>(size + 2, std::vector<double>(size + 2, 0.0));
+        u = std::vector<std::vector<double>>(size + 2, std::vector<double>(size + 2, 0.0));
     
         double sum = 0.0;
-
-        for (int i = 1; i < size + 1; i++) {
-            for (int j = 1; j < size + 1; j++) {
-                f[i][j] = fun_f(i * h, j * h);
-            }
-        }
-
-        for (int i = 0; i < size + 2; i++) {
-            sum += fun_g(i * h, 0) + fun_g(i * h, (size + 1) * h) + fun_g(0, i * h) + fun_g((size + 1) * h, i * h);
-        }
-        sum -= fun_g(0, 0) + fun_g(0, (size + 1) * h) + fun_g((size + 1) * h, 0) + fun_g((size + 1) * h, (size + 1) * h);
-        u = std::vector<std::vector<double>>(size + 2, std::vector<double>(size + 2, sum / (4 * (size + 1))));
 
         for (int i = 0; i < size + 2; i++) {
             u[i][0] = fun_g(i * h, 0);
             u[i][size + 1] = fun_g(i * h, (size + 1) * h);
             u[0][i] = fun_g(0, i * h);
             u[size + 1][i] = fun_g((size + 1) * h, i * h);
+            sum += fun_g(i * h, 0) + fun_g(i * h, (size + 1) * h) + fun_g(0, i * h) + fun_g((size + 1) * h, i * h);
+        }
+
+        sum = (sum - fun_g(0, 0) - fun_g(0, (size + 1) * h) - fun_g((size + 1) * h, 0) - fun_g((size + 1) * h, (size + 1) * h)) / (4 * (size + 1));
+
+        for (int i = 1; i < size + 1; i++) {
+            for (int j = 1; j < size + 1; j++) {
+                f[i][j] = fun_f(i * h, j * h);
+                u[i][j] = sum;
+            }
         }
     }
 
@@ -91,12 +90,12 @@ public:
     }
 
     double test_results() {
-        const double zero  = 10e-16;
+        const double zero  = 10e-15;
         double sum_error = 0.0;
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
                 if (fabs(fun(i * h, j * h)) > zero)
-                    sum_error += fabs((u[i][j] - fun(i * h, j * h)) / fun(i * h, j * h));
+                    sum_error += fabs((u[i][j] - fun(i * h, j * h))) / fabs(fun(i * h, j * h));  
             }
         }
         return sum_error / (size * size);
@@ -104,17 +103,21 @@ public:
 };
 
 int main() {
-    const int threads_num = 4;
+    const int threads_num = 6;
     omp_set_num_threads(threads_num);
 
-    auto fun = [](double x, double y) { return x * x + y; };
-    auto fun_d = [](double x, double y) { return 2.0; };
-    wavefront_approximator net(100, fun, fun_d);
+    auto fun = [](double x, double y) { return exp(-x * pow(y, 3)) ; };
+    auto fun_d = [](double x, double y) { return pow(y, 6) * exp(-x * pow(y, 3)) + 3 * x * exp(-x * pow(y, 3)) * (3 * x * pow(y, 3) - 2); };
 
-    auto start_time = omp_get_wtime();
-    net.process_net();
-    auto end_time = omp_get_wtime();
+    std::vector<int> test_net_sizes{20, 40, 80, 160, 320};
 
-    std::cout << end_time - start_time << std::endl;
-    std::cout << net.test_results() << std::endl;
+    for (auto it: test_net_sizes) {
+        wavefront_approximator net(it, fun, fun_d);
+        auto start_time = omp_get_wtime();
+        net.process_net();
+        auto end_time = omp_get_wtime();
+
+        std::cout << end_time - start_time << std::endl;
+        std::cout << net.test_results() << std::endl;
+    }
 }
